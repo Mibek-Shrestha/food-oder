edd520e44bbc2893277925af5be6d007efbf8a19
mibek-shrestha <mibekstha@gmail.com>
2023-06-01 17:36:08 +0545

    meal file is added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../assests/meals.jpg';
 import classes from './Header.module.css';
-const Header = (props) => {
-  return <React.Fragment>
-    <header className={classes.header}>
-        <h1>React Meals</h1>
-        <button>Cart</button>
-   
-    </header>
-    <div className={classes['main-image']}>
-         <img src={mealsImage} alt='A table full of meal'/>
-    </div>
-      
-  </React.Fragment>
-}
 
-export default Header
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
