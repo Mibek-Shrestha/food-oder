559e8ceeed984ae11158d02c2feffeb54703d5db
mibek-shrestha <mibekstha@gmail.com>
2023-06-01 18:53:46 +0545

    Cart file is added

---
+++ src/components/UI/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Input.module.css'
+const Input = (props) => {
+  return <div className="input">
+    <label htmlFor="props.input.id">{props.label}</label>
+    <input id={props.input.id} {...props.input} />
+  </div>
+}
+
+export default Input
