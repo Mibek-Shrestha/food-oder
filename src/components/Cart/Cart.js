2eb8bef87b313f0912eb307e3affb2e0d32a115b
mibek-shrestha <mibekstha@gmail.com>
2023-06-04 14:46:36 +0545

    useContext

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
-const Cart = () => {
+const Cart = (props) => {
    const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
@@ -9,7 +9,7 @@ const Cart = () => {
       ))}
     </ul>
   ); 
-  return <Modal>
+  return <Modal onClose={props.onClose}>
     {cartItems} 
     <div className={classes.total}>
       <span>
@@ -18,7 +18,7 @@ const Cart = () => {
       <span>35.62</span>
       </div>
     <div className={classes.action}>
-      <button className={classes['button--alt']}>Close</button>
+      <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
       <button className={classes.button}>Order</button>
     </div>
   </Modal>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
+import { useContext } from 'react'; // import useContext
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../store/cart-context'
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext); // use useContext
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0); // use reduce to get the total number of items in the cart
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
